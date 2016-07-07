d8dea37e6e75c400af426c8923142f2d8d9a833f
Choijm0715 <choijm0715@nate.com>
2016-07-07 16:58:44 +0900

    160707 작업

---
+++ app/src/main/java/com/example/user/userver/Main2Activity.java
@@ -0,0 +1,40 @@
+package com.example.user.userver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+        String pwd = intent.getStringExtra("pwd");
+
+        TextView tv = (TextView)findViewById(R.id.View_id);
+        tv.setText(id + "\n" + pwd);
+
+
+    }
+
+}
